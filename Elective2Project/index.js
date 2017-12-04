9d1126c489fa41639b29940d9a23015592610890
cranksgolem <33646994+cranksgolem@users.noreply.github.com>
2017-12-04 10:43:44 +0800

    Added Date Entry
    Date entry is added to home page. Page switching is also partially implemented.

--- Elective2Project/index.js
+++ Elective2Project/index.js
@@ -6,10 +6,42 @@ document.addEventListener('deviceready', onDeviceReady, false);
 	
 	function on_save()
 	{
-		var company = document.getElementById("company").value;
+		var company = document.getElementById("selcompany").value;
 		var hour = document.getElementById("hour").value;
 		var minute = document.getElementById("minute").value;
 		var second = document.getElementById("second").value;
 		
 		alert("Company: " + company + " " + hour + ":" + minute + ":" + second);
+	}
+	
+	function set_current_date() 
+	{
+		console.log("set_current_date()");
+
+		var date = new Date; //set the current date
+		var year = date.getFullYear();
+		var month = date.getMonth()+1;
+		var day = date.getDate();
+
+		if (month.toString().length == 1)
+			month = "0" + month;
+		if (day.toString().length == 1)
+			day = "0" + day;
+
+		date = year + "-" + month + "-" + day;
+		
+		document.getElementById("date").value = date;
+	}
+	
+	function open_home()
+	{
+		document.getElementById("home").style.display = "block";
+		document.getElementById("company").style.display = "none";
+		
+	}
+	
+	function open_company()
+	{
+		document.getElementById("home").style.display = "none";
+		document.getElementById("company").style.display = "block";
 	}
