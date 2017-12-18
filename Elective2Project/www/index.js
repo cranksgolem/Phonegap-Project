2ef20d7b69afe4ecb0a401bb4d38181876a383a0
cranksgolem <kimvinson1002@gmail.com>
2017-12-18 09:25:06 +0800

    Pretty Much Finished
    All functions are working.
    Bug on "Companies" screen (Some work hours can't be seen when list gets too big).

--- Elective2Project/www/index.js
+++ Elective2Project/www/index.js
@@ -38,6 +38,10 @@ document.addEventListener('deviceready', onDeviceReady, false);
 		selectedcompany.WorkHours.push(newworkhour);
 		var newJson = JSON.stringify(myObj);
 		window.localStorage.setItem("arraycompany", newJson);
+		
+		document.getElementById("hour").value = "";
+		document.getElementById("minute").value = "";
+		document.getElementById("second").value = "";
 	}
 	
 	function set_current_date() 
@@ -159,7 +163,6 @@ document.addEventListener('deviceready', onDeviceReady, false);
 			newlabel2.style = "display:inline-block;padding-top:3px;width:52%;color:#010513;";
 			newicon.style = "display:inline-block;padding-top: 3px;width:10%;";
 			newicon.className = "glyphicon glyphicon-remove";
-			//newicon.innerHTML = companyarray.array[y].innerHTML;
 			newicon.id = companyarray.array[y].innerHTML;
 			newicon.onclick = remove_company;
 			newdiv.appendChild(newlabel1);
@@ -178,8 +181,11 @@ document.addEventListener('deviceready', onDeviceReady, false);
 		var listworkhours = document.getElementById("list_work_hours");
 		var companyarrayjson = window.localStorage.getItem("arraycompany");
 		var companyarray = JSON.parse(companyarrayjson);
+		var companiestotalamount = 0;
 		var y;
 		var x;
+		
+		listworkhours.innerHTML = '';
 		
 		if (companyarray.array.length != 0)
 		{	
@@ -190,8 +196,8 @@ document.addEventListener('deviceready', onDeviceReady, false);
 				var newmaindiv = document.createElement("div");
 				var newdivheader = document.createElement("div");
 				var newdivlistworkhours = document.createElement("div");
-				var totalhours;
-				var totalamount;
+				var totalhours = 0;
+				var totalamount = 0;
 				
 				if (currentcompany.WorkHours.length != 0)
 				{
@@ -201,24 +207,92 @@ document.addEventListener('deviceready', onDeviceReady, false);
 						var hourslabel = document.createElement("label");
 						var date = document.createElement("label");
 						var amount = document.createElement("amount");
+						var deleteicon = document.createElement("i");
+						var brk = document.createElement("br");
 						
-						hourslabel.innerHTML = currency(currentcompany.WorkHours[x].Hours);
-						amount.innerHTML = currency(currentcompany.WorkHours[x].Amount);
+						hourslabel.innerHTML = currency(currentcompany.WorkHours[x].Hours) + " hours";
+						amount.innerHTML = "P" + currency(currentcompany.WorkHours[x].Amount);
 						date.innerHTML = currentcompany.WorkHours[x].Datee;
 						
+						newdivworkhour.style = "display:block;padding: 10px;background: #D1D6E4;border-radius: 8px;margin-left: 10px;margin-right: 10px;margin-top: 10px;";
+						hourslabel.style = "display:inline-block;text-align: left;padding: 4px;color: black;font-size:large;width:95%;";
+						date.style = "display:inline-block;text-align: left;padding: 4px;color: grey;width:50%;";
+						amount.style = "display:inline-block;text-align: right;padding: 4px;color: black;width:50%";
+						
+						deleteicon.style = "display:inline-block;padding-top: 3px;width:5%;";
+						deleteicon.className = "glyphicon glyphicon-remove";
+						deleteicon.id = currency(currentcompany.WorkHours[x].Hours) + currency(currentcompany.WorkHours[x].Amount) + currentcompany.WorkHours[x].Datee;
+						deleteicon.onclick = remove_work_hour;
+						
 						newdivworkhour.appendChild(hourslabel);
+						newdivworkhour.appendChild(deleteicon);
+						newdivworkhour.appendChild(brk);
 						newdivworkhour.appendChild(date);
 						newdivworkhour.appendChild(amount);
 						
 						newdivlistworkhours.appendChild(newdivworkhour);
+						
+						totalhours = totalhours + currentcompany.WorkHours[x].Hours;
+						totalamount = totalamount + currentcompany.WorkHours[x].Amount;
 					}
 				}
+				
+				var companyheader = document.createElement("div");
+				var totalhoursheader = document.createElement("div");
+				var totalamountheader = document.createElement("div");
+				
+				companyheader.innerHTML = currentcompany.innerHTML;
+				totalhoursheader.innerHTML = "Total Hours: "+ currency(totalhours);
+				totalamountheader.innerHTML = "Amount: P" + currency(totalamount);
+				
+				companyheader.style = "display:inline-block;padding:4px;margin:4px;font-size:x-large;width:30%;font-Family: Times New Roman;";
+				totalhoursheader.style = "display:inline-block;padding:4px;margin:4px;font-size:small;width:30%;text-align:right;";
+				totalamountheader.style = "display:inline-block;padding:4px;margin:4px;font-size:small;width:30%;text-align:right;";
+				
+				newdivheader.appendChild(companyheader);
+				newdivheader.appendChild(totalhoursheader);
+				newdivheader.appendChild(totalamountheader);
 				
 				newmaindiv.appendChild(newdivheader);
 				newmaindiv.appendChild(newdivlistworkhours);
 				listworkhours.appendChild(newmaindiv);
+				
+				companiestotalamount = companiestotalamount + totalamount;
 			}
 		}
+		
+		var comptotalamountdisplay = document.getElementById("companies_total_amount_display");
+		comptotalamountdisplay.innerHTML = "P" + currency(companiestotalamount);
+	}
+	
+	function remove_work_hour()
+	{
+		console.log("Remove_work_hour");
+		var sourcename = event.target || event.srcElement;
+		var companyarrayjson = window.localStorage.getItem("arraycompany");
+		var companyarray = JSON.parse(companyarrayjson);
+		var x;
+		var y;
+		
+		for (y=0 ; y < companyarray.array.length; y++)
+		{
+			var currentcompany = companyarray.array[y];
+			
+			for (x = 0; x < currentcompany.WorkHours.length; x++)
+			{
+				var comparestring = currency(currentcompany.WorkHours[x].Hours) + currency(currentcompany.WorkHours[x].Amount) + currentcompany.WorkHours[x].Datee;
+				if (sourcename.id == comparestring)
+				{
+					currentcompany.WorkHours.splice(x,1);
+					break;
+				}
+			}
+		}
+		
+		var newcompanyarrayjson = JSON.stringify(companyarray);
+		window.localStorage.setItem("arraycompany", newcompanyarrayjson);
+		
+		set_company_screen();
 	}
 	
 	function display_company_screen()
@@ -302,4 +376,7 @@ document.addEventListener('deviceready', onDeviceReady, false);
 		
 		document.getElementById("add_form").style.display = "none";
 		document.getElementById("show_form_link").style.display = "block";
+		
+		document.getElementById("company_name").value = "";
+		document.getElementById("company_rate").value = "";
 	}
